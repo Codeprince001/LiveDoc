eeb154ea96fc7aff3ce69780f774069d9de8cd04
Codeprince001 <urangyorkwisdom@gmail.com>
2024-08-24 18:24:02 +0100

    set-up liveblock Api route

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -1,37 +1,35 @@
 import { liveblocks } from "@/lib/liveblocks";
 import { getUserColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
-import { colors } from "@clerk/themes/dist/clerk-js/src/ui/foundations/colors";
-import { Liveblocks } from "@liveblocks/node";
 import { redirect } from "next/navigation";
 
+export async function POST(request: Request) {
+  const clerkUser = await currentUser();
 
-  export async function POST(request: Request) {
-    const clerkUser = await currentUser()
+  if(!clerkUser) redirect('/sign-in');
 
-    if(!clerkUser) redirect("/sign-in")
+  const { id, firstName, lastName, emailAddresses, imageUrl } = clerkUser;
 
-    const {id, firstName, lastName, emailAddresses, imageUrl} = clerkUser
-    // Get the current user from your database
-    const user = {
+  // Get the current user from your database
+  const user = {
+    id,
+    info: {
       id,
-      info: {
-        id,
-        name: `${firstName} ${lastName}`,
-        email: emailAddresses[0].emailAddress,
-        avatar: imageUrl,
-        color: getUserColor(id)
-      }
+      name: `${firstName} ${lastName}`,
+      email: emailAddresses[0].emailAddress,
+      avatar: imageUrl,
+      color: getUserColor(id),
     }
-  
-    // Identify the user and return the result
-    const { status, body } = await liveblocks.identifyUser(
-      {
-        userId: user.info.email,
-        groupIds: []
-      },
-      { userInfo: user.info },
-    );
-  
-    return new Response(body, { status });
+  }
+
+  // Identify the user and return the result
+  const { status, body } = await liveblocks.identifyUser(
+    {
+      userId: user.info.email,
+      groupIds: [],
+    },
+    { userInfo: user.info },
+  );
+
+  return new Response(body, { status });
 }
