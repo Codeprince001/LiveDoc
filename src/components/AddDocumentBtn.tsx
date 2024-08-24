7f8dbb2ce04da08990486cb006b840729c343488
Codeprince001 <urangyorkwisdom@gmail.com>
2024-08-24 18:12:41 +0100

    created Add new Document component

---
+++ src/components/AddDocumentBtn.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { Button } from './ui/button'
+import Image from 'next/image'
+import addBtn from "../../public/assets/icons/add.svg"
+import { createDocument } from '@/lib/actions/room.actions'
+import { useRouter } from 'next/navigation'
+
+const AddDocumentBtn = ({userId, email}: AddDocumentBtnProps) => {
+  
+  const router = useRouter()
+  
+  const addDocumentHandler = async () => {
+
+    try {
+      const room = await createDocument({userId, email})
+
+      if(room) router.push(`document/${room.id}`)
+
+    } catch(error){
+      console.log(error)
+    }
+  }
+  
+
+  return (
+    <Button type='submit' onClick={addDocumentHandler} className='gradient-blue flex gap-1 shadow-md'>
+      <Image
+        alt='add'
+        src={addBtn}
+        width={24}
+        height={24}
+      />
+      <p className='hidden sm:block'>Start a black document</p>
+    </Button>
+  )
+}
+
+export default AddDocumentBtn
