f51165231b8cc27a85965fad070b5e39f7b529fa
Codeprince001 <urangyorkwisdom@gmail.com>
2024-08-24 18:26:03 +0100

    set-up document page

--- src/app/(root)/document/[id]/page.tsx
+++ src/app/(root)/document/[id]/page.tsx
@@ -1,10 +1,41 @@
+import { getDocument } from "@/lib/actions/room.actions";
+import { getClerkUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
+import CollaborativeRoom from "@/components/CollaborativeRoom";
 
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if(!clerkUser) redirect('/sign-in');
 
-const Document = () => {
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if(!room) redirect('/');
+
+  const userIds = Object.keys(room.usersAccesses);
+  const users = await getClerkUsers({ userIds });
+
+  const usersData = users.map((user: User) => ({
+    ...user,
+    userType: room.usersAccesses[user.email]?.includes('room:write')
+      ? 'editor'
+      : 'viewer'
+  }))
+
+  const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes('room:write') ? 'editor' : 'viewer';
+
   return (
-    <div>
-     
-    </div>
+    <main className="flex w-full flex-col p-2 justify-between items-center">
+      <CollaborativeRoom 
+        roomId={id}
+        roomMetadata={room.metadata}
+        users={usersData}
+        currentUserType={currentUserType}
+      />
+    </main>
   )
 }
 
